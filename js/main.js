7e05f95a8ca5d6c576a7dd4255ec4b86363f0dc4
Paul <pwittschen@gmail.com>
2015-06-05 12:40:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(function() {
 	$('.sound_button').on('click', function() {
-		var audio_id = $(this).attr('data-soundID');
-		play_sample(audio_id);
+		if ($(this).attr('id') === 'b_ryuko_1') {
+			if (window.confirm("Do you have your tissues ready?")) {
+				var audio_id = $(this).attr('data-soundID');
+				play_sample(audio_id);		
+			}
+		} else {
+			var audio_id = $(this).attr('data-soundID');
+			play_sample(audio_id);		
+		}
+
 	});
 });
 function play_sample(audio_id) {
